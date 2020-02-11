62f128ca8b579ca56bf1ac734852104c7123e990
Gaubee <kezhaofeng@bnqkl.cn>
2020-02-11 10:30:59 +0800

    :bug: 修复ed25519的sign错误

--- packages/crypto-tweetnacl/src/sign.ts
+++ packages/crypto-tweetnacl/src/sign.ts
@@ -389,7 +389,7 @@ function modL(r: ByteArray, x: NumArray) {
 
   for (j = 0; j < 32; j++) {
     x[j] += carry - (x[31] >> 4) * L[j];
-    carry = x[j] >> 8;
+    carry = Math.floor((x[j] + 128) / 256);
     x[j] &= 255;
   }
 
