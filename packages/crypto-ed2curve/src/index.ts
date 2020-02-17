c7a77795049a756dbe324884fc62f94859067e20
Gaubee <kezhaofeng@bnqkl.cn>
2020-02-17 22:14:20 +0800

    :bug: 修复tweetnacl导出keypairHelper没有使用BBuffer的问题

--- packages/crypto-ed2curve/src/index.ts
+++ packages/crypto-ed2curve/src/index.ts
@@ -1,6 +1,6 @@
+import type {} from "@bfchain/core-typings";
 export * from "./ed2curve";
 import { convertPublicKey, convertSecretKey } from "./ed2curve";
-import type {} from "@bfchain/core-typings";
 
 export const ed2curveHelper: BFChainCore.Ed2curveHelperInterface = {
   convertPublicKey,
--- packages/crypto-tweetnacl/src/index.ts
+++ packages/crypto-tweetnacl/src/index.ts
@@ -1,3 +1,4 @@
+import type {} from "@bfchain/core-typings";
 export * from "./sign";
 export * from "./box";
 export * from "./core";
@@ -6,18 +7,18 @@ export * from "./curve25519";
 
 import { sign_detached, sign_detached_verify, sign_keyPair_fromSeed } from "./sign";
 import { BoxLength, box, box_open } from "./box";
-import type {} from "@bfchain/core-typings";
+import { BBuffer } from "@bfchain/util-buffer";
 
 export const keypairHelper: BFChainCore.KeypairHelperInterface = {
   create(secretHash: Uint8Array) {
     const keypair = sign_keyPair_fromSeed(secretHash);
     return {
-      secretKey: Buffer.from(keypair.secretKey),
-      publicKey: Buffer.from(keypair.publicKey),
+      secretKey: BBuffer.from(keypair.secretKey),
+      publicKey: BBuffer.from(keypair.publicKey),
     };
   },
   detached_sign(hash: Uint8Array, secretKey: Uint8Array) {
-    return Buffer.from(sign_detached(hash, secretKey));
+    return BBuffer.from(sign_detached(hash, secretKey));
   },
   detached_verify(hash: Uint8Array, signatureBuffer: Uint8Array, publicKey: Uint8Array) {
     return sign_detached_verify(hash, signatureBuffer, publicKey);
