635075c568ba66ca47d4503fc353d7b4917ec3ef
chenyangfan <chenyangfan@ifmchain.local>
2020-06-02 10:36:30 +0800

    更改钩子

--- packages/channel/src/@types.ts
+++ packages/channel/src/@types.ts
@@ -18,7 +18,7 @@ declare namespace BFChainCore {
     endBroadcast: BFChainUtil.EventInOut<{ duraction: number }, any>;
   };
   type ChainChannelHanlderEventMap = {
-    beforeRequestWithBinaryData: BFChainUtil.EventInOut<[import('@bfchain/core-model').DUPLEX_API_CMD, Uint8Array], void>,
+    afterRequestWithBinaryData: BFChainUtil.EventInOut<[import('@bfchain/core-model').DUPLEX_API_CMD, any], void>,
     handleMessageError: {
       in: { handleName: string; error: Error };
       out: undefined;
