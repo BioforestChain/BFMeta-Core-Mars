fe392c1a023dffdc9a170abf8157573763e9e5b9
Gaubee <kezhaofeng@bnqkl.cn>
2020-07-07 21:27:45 +0800

    :bug: 修复TransactionQueryOptions toJSON的异常

--- packages/model-channel/src/transaction.chainChannel.model.ts
+++ packages/model-channel/src/transaction.chainChannel.model.ts
@@ -61,7 +61,7 @@ export class TransactionQueryOptions extends Message<TransactionQueryOptions>
   /**购买的 dappid */
   @Field.d(TransactionQueryOptions.INC++, "string", "optional")
   purchaseDAppid?: string;
-  /**购买的 dappid */
+  /**在range中的元素 */
   @Field.d(TransactionQueryOptions.INC++, "string", "optional")
   range?: string;
   /**查询结果分页：起始下标 */
@@ -81,23 +81,25 @@ export class TransactionQueryOptions extends Message<TransactionQueryOptions>
     return (res as unknown) as T;
   }
   toJSON() {
-    const res: BFChainCore.TransactionQueryOptionsJSON = { offset: this.offset };
-    this.type && (res.type = this.type);
-    this.signature && (res.signature = this.signature);
-    this.senderId && (res.senderId = this.senderId);
-    this.recipientId && (res.recipientId = this.recipientId);
-    this.dappid && (res.dappid = this.dappid);
-    this.lns && (res.lns = this.lns);
-    this.storage && (res.storage = this.storage);
-    this.blockSignature && (res.blockSignature = this.blockSignature);
-    this.minHeight && (res.minHeight = this.minHeight);
-    this.maxHeight && (res.maxHeight = this.maxHeight);
-    this.numberOfSenderTransactions &&
-      (res.numberOfSenderTransactions = this.numberOfSenderTransactions);
-    this.trusteeId && (res.trusteeId = this.trusteeId);
-    this.purchaseDAppid && (res.purchaseDAppid = this.purchaseDAppid);
-    this.range && (res.range = this.range);
-    this.offset && (res.offset = this.offset);
+    const res: BFChainCore.TransactionQueryOptionsJSON = {
+      type: this.type,
+      signature: this.signature,
+      senderId: this.senderId,
+      recipientId: this.recipientId,
+      dappid: this.dappid,
+      lns: this.lns,
+      storage: this.storage,
+      blockSignature: this.blockSignature,
+      minHeight: this.minHeight,
+      maxHeight: this.maxHeight,
+      numberOfSenderTransactions: this.numberOfSenderTransactions,
+      trusteeId: this.trusteeId,
+      purchaseDAppid: this.purchaseDAppid,
+      range: this.range,
+      offset: this.offset,
+      limit: this.limit,
+    };
+
     return res;
   }
 }
