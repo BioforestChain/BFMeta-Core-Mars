c136577d173166aa44c262be1e3cda2b422c9662
wuzuxian <wzx@ifmchain.local>
2020-09-21 16:12:49 +0800

    :bug:查询事件排序类型修改

--- packages/model-channel/src/@types.ts
+++ packages/model-channel/src/@types.ts
@@ -58,7 +58,7 @@ declare namespace BFChainCore {
   /**查询交易的排序选项 */
   type TransactionSortOptionsJSON = {
     /**根据链上事件索引 */
-    tIndex?: -1 | 1;
+    tIndex: -1 | 1;
     /**根据交易时间戳排序 */
     // index?: -1 | 1;
     // height?: -1 | 1;
