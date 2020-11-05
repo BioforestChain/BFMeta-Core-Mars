d489b65403f8eb477737c428f0402c980f816f19
wmc <wengmaochun@bnqkl.cn>
2020-11-05 19:50:04 +0800

    :bug: block statistic error

--- packages/helper-block_base_statistics/src/blockBaseStatisticsHelper.ts
+++ packages/helper-block_base_statistics/src/blockBaseStatisticsHelper.ts
@@ -80,6 +80,7 @@ export class BlockBaseStatisticsHelper {
     // 统计资产信息
     const assetStatistic = statistics_info.initAssetStatistic(assetInfo);
     assetStatistic.total.changeAmount = assetStatistic.total.changeAmount + sourceAmount;
+    assetStatistic.total.moveAmount = assetStatistic.total.moveAmount + sourceAmount;
     assetStatistic.total.changeCount += 1;
     assetStatistic.total.addTransactionCount(transaction.signature);
     /**需要统计的交易类型 */
