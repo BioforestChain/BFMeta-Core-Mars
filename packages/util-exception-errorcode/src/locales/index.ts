f765546cebc2509c964d6b5f0e2d942d652155de
wmc <wengmaochun@bnqkl.cn>
2022-01-12 17:59:30 +0800

    :bug: 获取所有的错误码

--- packages/util-exception-errorcode/src/locales/index.ts
+++ packages/util-exception-errorcode/src/locales/index.ts
@@ -8,3 +8,5 @@ translatedMap.set(I18N_LANGUAGE_TYPE.ENGLISH, ERROR_CODE_LIST_EN);
 translatedMap.set(I18N_LANGUAGE_TYPE.CHINESE, ERROR_CODE_LIST_ZH);
 
 export const translatedErrorCodeListMap = translatedMap;
+
+export const ERROR_CODE_LIST = Object.keys(ERROR_CODE_LIST_EN);
