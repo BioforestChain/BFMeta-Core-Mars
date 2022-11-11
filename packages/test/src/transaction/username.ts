951f920fa7a3d52cb85b5e42e454d7f90ea90869
wuzuxian <wzx@ifmchain.local>
2022-11-11 17:09:10 +0800

    1

--- packages/test/src/transaction/username.ts
+++ packages/test/src/transaction/username.ts
@@ -16,20 +16,24 @@ async function getUsernameTransaction(sender: AccountModel, bfchainCore: BFChain
   const keypair = await bfchainCore.accountBaseHelper.createSecretKeypair(sender.secret);
   const data: BFChainCore.TxBodyJSON = {
     version: bfchainCore.config.version,
+    subEnvParams: {},
+
     type: bfchainCore.transactionHelper.USERNAME, // 交易类型
     senderId: sender.address, // 发起者地址
     senderPublicKey: sender.publicKey, // 发起者公钥
     senderSecondPublicKey: "", // 发起者二次公钥
+    maxFee: "100000000",
     rangeType: RANGE_TYPE.EMPTY,
     range: [],
-    timestamp: 770880, // 生成交易时间戳
-    fee: "78622", // 交易手续费
     remark: { remark: "body.remark" }, // 交易备注，任意信息
     dappid: getRandomDAppid(), // 交易所属的 dappid
     lns: bfchainCore.config.genesisLocationName,
-    sourceIP: "127.0.0.1", // 交易来源 ip
     fromMagic: bfchainCore.config.magic, // 交易来源链的 magic
     toMagic: bfchainCore.config.magic, // 交易去往链的 magic
+
+    timestamp: 770880, // 生成交易时间戳
+    fee: "78622", // 交易手续费
+    sourceIP: "127.0.0.1", // 交易来源 ip
     applyBlockHeight: 10086, // 交易发起高度
     effectiveBlockHeight: 10100,
     storage: {
--- packages/test/src/transaction/vote.ts
+++ packages/test/src/transaction/vote.ts
@@ -12,21 +12,25 @@ async function getVoteTransaction(sender: AccountModel, bfchainCore: BFChainCore
   const keypair = await bfchainCore.accountBaseHelper.createSecretKeypair(sender.secret);
   const data: BFChainCore.TxBodyJSON = {
     version: bfchainCore.config.version,
+
+    subEnvParams: {},
     type: bfchainCore.transactionHelper.VOTE, // 交易类型
     senderId: sender.address, // 发起者地址
     senderPublicKey: sender.publicKey, // 发起者公钥
     senderSecondPublicKey: "", // 发起者二次公钥
+    maxFee: "100000000",
     recipientId: getGenesisAccount().address,
     rangeType: RANGE_TYPE.EMPTY,
     range: [], // 被投票人的地址
-    timestamp: 770880, // 生成交易时间戳
-    fee: "78622", // 交易手续费
     remark: { remark: "body.remark" }, // 交易备注，任意信息
     dappid: getRandomDAppid(), // 交易所属的 dappid
     lns: bfchainCore.config.genesisLocationName,
-    sourceIP: "127.0.0.1", // 交易来源 ip
     fromMagic: bfchainCore.config.magic, // 交易来源链的 magic
     toMagic: bfchainCore.config.magic, // 交易去往链的 magic
+
+    timestamp: 770880, // 生成交易时间戳
+    fee: "78622", // 交易手续费
+    sourceIP: "127.0.0.1", // 交易来源 ip
     applyBlockHeight: 10086, // 交易发起高度
     effectiveBlockHeight: 10100,
   };
