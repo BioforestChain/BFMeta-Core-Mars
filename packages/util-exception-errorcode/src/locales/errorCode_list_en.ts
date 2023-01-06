711e96e15172f13965ee6b1f77149a995d0682c6
wmc <wengmaochun@bnqkl.cn>
2023-01-06 18:14:18 +0800

    :bug: 完善报错信息

--- packages/util-exception-errorcode/src/locales/errorCode_list_en.ts
+++ packages/util-exception-errorcode/src/locales/errorCode_list_en.ts
@@ -149,7 +149,8 @@ export const ERROR_CODE_LIST_EN = {
   "001-11069": "Account already has a username, errorId {errorId}",
   "001-11070": "Issue entity times use up, entityFactory {entityFactory}",
   "001-11071": "Asset already exist, magic {magic} assetType {assetType}",
-  "001-11072": "Frozen asset with signature {signature} in blockChain not exist or expired",
+  "001-11072":
+    "Frozen asset with signature {signature} assetType ${assetType} in blockChain not exist or expired",
   "001-11073": "Not enough issue entity times, entityFactory {entityFactory}",
 
   // block logic
