0b06ca71edf4b6aff34288906d04ec23089479a1
wmc <wengmaochun@bnqkl.cn>
2023-03-15 21:20:39 +0800

    :bug: 更新依赖

--- packages/test/src/transaction/transferAsset.ts
+++ packages/test/src/transaction/transferAsset.ts
@@ -14,6 +14,7 @@ import {
   getRandomDAppid,
 } from "../include";
 import { sleep } from "@bfchain/util";
+import{Long} from "@bfchain/protobuf"
 
 const _powCount: { [add: string]: number } = {};
 function getPOWInfo<T extends Transaction>(address: string) {
@@ -118,14 +119,71 @@ async function getTransferAssetTransaction(sender: AccountModel, bfchainCore: BF
 (async () => {
   const bfchainCore = await getBfchainCoreEntry();
 
-  await getTransferAssetTransaction(getSenderWithoutSecondSecret(), bfchainCore);
-  await getTransferAssetTransaction(getSenderWithSecondSecret(), bfchainCore);
+  const xx: BFChainCore.BlockJSON = {
+    version: 1,
+    height: 14,
+    blockSize: 519,
+    generatorPublicKey: "879485f07b711ce37a366bf8fccb380a37479fc62becb552de97c83488f10cc8",
+    generatorSecondPublicKey: "",
+    generatorEquity: "0",
+    previousBlockSignature:
+      "97fb45b2f8c43be3556f42d8fc049c67668d9ce0d78e9b76f039175771a8e3911cfd397a554c2d513f0eb5a7ba667c12870d7e75737dcbc0d264af3645ab530c",
+    timestamp: 11572350,
+    reward: "4000000000",
+    magic: "V7U7T",
+    remark: {
+      info: "the net version is testnet, only running for the test",
+      debug: "BFMTEST_win32_v3.7.1_P13_DP1_T0_C0_A0.00 UNTRS_B0_E0_TIME9 LOST 0",
+    },
+    asset: {},
+    transactionInfo: {
+      startTindex: 576,
+      numberOfTransactions: 0,
+      payloadHash: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+      payloadLength: 0,
+      blobSize: 0,
+      totalAmount: "0",
+      totalFee: "0",
+      transactionInBlocks: [],
+      statisticInfo: {
+        totalFee: "0",
+        totalAsset: "0",
+        totalChainAsset: "0",
+        totalAccount: 0,
+        magicAssetTypeTypeStatisticHashMap: {},
+      },
+    },
+    roundOfflineGeneratersHashMap: {},
+    blockParticipation: "0",
+    signature:
+      "a5cfbe59fc2401c6160a4a0d657d4fb86a6a57cbe0f2529bc08323dd23e3f13c80b7947ea9d7146f46c8751dd167916cded79675ed9ac6d31b73acf344db9003",
+    signSignature: "",
+  };
 
-  console.log(bfchainCore.config.version);
+  // const xx: BFChainCore.BlockJSON = require(process.cwd() +
+  //   "/assets/bfm-genesisBlock-mainnet.json");
 
-  bfchainCore.patchInstaller.changeHeight(50000000);
+  const xxx = await bfchainCore.block.recombineBlock(xx);
 
-  await sleep(1000);
+  // console.log(xxx.transactionInfo);
 
-  console.log(bfchainCore.config.version);
+  const yy = bfchainCore.block.getBlockFactoryFromHeight(xxx.height);
+
+  // debugger
+  yy.commonBlockVerify.verifyBlockSize(xxx);
+
+  xxx.transactionInfo.blobSize = 0;
+  // console.log(xxx.transactionInfo);
+  // console.log(Long.fromNumber(0))
+
+  // await getTransferAssetTransaction(getSenderWithoutSecondSecret(), bfchainCore);
+  // await getTransferAssetTransaction(getSenderWithSecondSecret(), bfchainCore);
+
+  // console.log(bfchainCore.config.version);
+
+  // bfchainCore.patchInstaller.changeHeight(50000000);
+
+  // await sleep(1000);
+
+  // console.log(bfchainCore.config.version);
 })();
