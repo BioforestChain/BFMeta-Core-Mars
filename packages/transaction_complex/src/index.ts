7d4bb4107a4f672d5dac067c25225501e8c5da3f
wmc <wengmaochun@bnqkl.cn>
2023-04-23 09:48:44 +0800

    :bug: 添加组合交易

--- packages/transaction_complex/src/index.ts
+++ packages/transaction_complex/src/index.ts
@@ -2,6 +2,7 @@ import { TRANSACTION_FACTORY_TYPES_MAP } from "@bfchain/core-transaction";
 import { TRANSACTION_TYPES_BASE } from "@bfchain/core-model";
 import { CustomTransactionFactory } from "./custom";
 import { RegisterChainTransactionFactory } from "./registerChain";
+import { MultipleTransactionFactory } from "./multiple";
 
 TRANSACTION_FACTORY_TYPES_MAP.VF.set(
   TRANSACTION_TYPES_BASE.CUSTOM,
@@ -19,7 +20,18 @@ TRANSACTION_FACTORY_TYPES_MAP.VF.set(
 TRANSACTION_FACTORY_TYPES_MAP.FV.set(
   RegisterChainTransactionFactory as BFChainCore.TransactionFactoryConstructor<any>,
   TRANSACTION_TYPES_BASE.REGISTER_CHAIN,
+);
+
+TRANSACTION_FACTORY_TYPES_MAP.VF.set(
+  TRANSACTION_TYPES_BASE.MULTIPLE,
+  MultipleTransactionFactory as BFChainCore.TransactionFactoryConstructor<any>,
+);
+TRANSACTION_FACTORY_TYPES_MAP.FV.set(
+  MultipleTransactionFactory as BFChainCore.TransactionFactoryConstructor<any>,
+  TRANSACTION_TYPES_BASE.MULTIPLE,
 );
 
 export * from "./custom";
 export * from "./registerChain";
+
+export * from "./multiple";
