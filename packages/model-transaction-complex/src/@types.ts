9da56f010785ea779ed42a60b948de6c85dd4598
wmc <wengmaochun@bnqkl.cn>
2023-05-15 18:15:51 +0800

    :bug: 添加承诺和承诺兑现交易

--- packages/model-transaction-complex/src/@types.ts
+++ packages/model-transaction-complex/src/@types.ts
@@ -26,4 +26,59 @@ declare namespace BFChainCore {
     multiple: MultipleJSON;
   }
   type MultipleTransactionJSON = TransactionMixJSON<MultipleAssetJSON, { hasRecipientId: false }>;
+
+  //#region 承诺交易
+  interface PromiseJSON {
+    /// 承诺兑现时才上链
+    transaction: BFChainCore.TransactionJSON;
+  }
+  interface PromiseAssetJSON {
+    promise: PromiseJSON;
+  }
+  type PromiseTransactionJSON = TransactionMixJSON<PromiseAssetJSON, { hasRecipientId: true }>;
+
+  interface PromiseResolveJSON {
+    promiseId: string;
+  }
+  interface PromiseResolveAssetJSON {
+    resolve: PromiseResolveJSON;
+  }
+  type PromiseResolveTransactionJSON = TransactionMixJSON<
+    PromiseResolveAssetJSON,
+    { hasRecipientId: true }
+  >;
+  //#endregion
+
+  //#region 宏交易
+  interface MacroJSON {
+    inputs: MacroJSON.Inputs[];
+    /// json 模板
+    transactionTemplate: string;
+  }
+  namespace MacroJSON {
+    interface BaseInput {
+      name: string;
+      pattern?: string; // regexp
+    }
+    interface TextInput extends BaseInput {}
+    interface AddressInput extends TextInput {}
+    interface SignatureInput extends TextInput {}
+
+    interface NumberInput extends BaseInput {
+      min?: number;
+      max?: number;
+      step?: number;
+    }
+
+    interface CalcInput extends NumberInput {
+      calc: string;
+    }
+
+    type Inputs = TextInput | AddressInput | SignatureInput | NumberInput | CalcInput;
+  }
+  interface MacroAssetJSON {
+    macro: MacroJSON;
+  }
+  type MacroTransactionJSON = TransactionMixJSON<MacroAssetJSON, { hasRecipientId: false }>;
+  //#endregion
 }
