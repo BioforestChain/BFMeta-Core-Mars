9683ffd7d9fc7dfc404e4967365c487db4dc5d07
wmc <wengmaochun@bnqkl.cn>
2023-05-15 18:15:51 +0800

    :bug: 添加承诺和承诺兑现交易

---
+++ packages/test/src/transaction/promise.ts
@@ -0,0 +1,214 @@
+import {
+  TransferAssetTransaction,
+  TransferAssetTransactionFactory,
+  RANGE_TYPE,
+  Transaction,
+  BFChainCore,
+  AcceptVoteTransaction,
+  AcceptVoteTransactionFactory,
+  PromiseTransaction,
+  PromiseTransactionFactory,
+} from "@bfchain/core";
+import {
+  getSenderWithSecondSecret,
+  getSenderWithoutSecondSecret,
+  getGenesisAccount,
+  AccountModel,
+  getBfchainCoreEntry,
+  getRandomDAppid,
+} from "../include";
+
+const _powCount: { [add: string]: number } = {};
+function getPOWInfo<T extends Transaction>(address: string) {
+  const count = _powCount[address] || 0;
+  _powCount[address] = count + 1;
+  const res: BFChainCore.TransactionPoWOptions<T> = {
+    count,
+    participation: "8888888" + "0".repeat(8),
+  };
+  return res;
+}
+
+async function getTransferAssetTransaction(sender: AccountModel, bfchainCore: BFChainCore) {
+  const keypair = await bfchainCore.accountBaseHelper.createSecretKeypair(sender.secret);
+  const data: BFChainCore.TxBodyJSON = {
+    version: bfchainCore.config.version,
+    type: bfchainCore.transactionHelper.TRANSFER_ASSET, // 交易类型
+    senderId: sender.address, // 发起者地址
+    senderPublicKey: sender.publicKey, // 发起者公钥
+    senderSecondPublicKey: "", // 发起者二次公钥
+    recipientId: getGenesisAccount().address,
+    rangeType: RANGE_TYPE.EMPTY,
+    range: [], // 接收资产账户地址
+    timestamp: 770880, // 生成交易时间戳
+    fee: "10", // 交易手续费
+    remark: { remark: "create transfer asset" }, // 交易备注，任意信息
+    dappid: getRandomDAppid(), // 交易所属的 dappid
+    lns: bfchainCore.config.genesisLocationName,
+    sourceIP: "127.0.0.1", // 交易来源 ip
+    fromMagic: bfchainCore.config.magic, // 交易来源链的 magic
+    toMagic: bfchainCore.config.magic, // 交易去往链的 magic
+    applyBlockHeight: 10086, // 交易发起高度
+    effectiveBlockHeight: 10100,
+    storage: {
+      key: "assetType",
+      value: "BFT",
+    },
+  };
+  let secondKeypair;
+  if (sender.secondSecret) {
+    secondKeypair = await bfchainCore.accountBaseHelper.createSecondSecretKeypair(
+      sender.secret,
+      sender.secondSecret,
+    );
+    data.senderSecondPublicKey =
+      await bfchainCore.accountBaseHelper.getPublicKeyStringFromSecondSecret(
+        sender.secret,
+        sender.secondSecret,
+      );
+  }
+  const pow =
+    data.applyBlockHeight > bfchainCore.config.tpowOfWorkExemptionBlocks
+      ? getPOWInfo<TransferAssetTransaction>(sender.address)
+      : undefined;
+  let trs = await bfchainCore.transaction.createTransaction<TransferAssetTransaction>(
+    TransferAssetTransactionFactory,
+    data,
+    {
+      transferAsset: {
+        sourceChainName: bfchainCore.config.chainName,
+        sourceChainMagic: bfchainCore.config.magic,
+        assetType: bfchainCore.config.assetType,
+        amount: "1000",
+      },
+    },
+    keypair,
+    secondKeypair,
+    undefined,
+    undefined,
+  );
+  if (pow) {
+    trs = await bfchainCore.transaction.transactionPowCalculator(trs, pow, keypair, secondKeypair);
+  }
+  return trs.toJSON();
+}
+
+async function getAcceptVoteTransaction(sender: AccountModel, bfchainCore: BFChainCore) {
+  const keypair = await bfchainCore.accountBaseHelper.createSecretKeypair(sender.secret);
+  const data: BFChainCore.TxBodyJSON = {
+    version: bfchainCore.config.version,
+    type: bfchainCore.transactionHelper.ACCEPT_VOTE, // 交易类型
+    senderId: sender.address, // 发起者地址
+    senderPublicKey: sender.publicKey, // 发起者公钥
+    senderSecondPublicKey: "", // 发起者二次公钥
+    rangeType: RANGE_TYPE.EMPTY,
+    range: [],
+    timestamp: 770880, // 生成交易时间戳
+    fee: "10", // 交易手续费
+    remark: { remark: "create accept vote" }, // 交易备注，任意信息
+    dappid: getRandomDAppid(), // 交易所属的 dappid
+    lns: bfchainCore.config.genesisLocationName,
+    sourceIP: "127.0.0.1", // 交易来源 ip
+    fromMagic: bfchainCore.config.magic, // 交易来源链的 magic
+    toMagic: bfchainCore.config.magic, // 交易去往链的 magic
+    applyBlockHeight: 10086, // 交易发起高度
+    effectiveBlockHeight: 10100,
+  };
+  let secondKeypair;
+  if (sender.secondSecret) {
+    secondKeypair = await bfchainCore.accountBaseHelper.createSecondSecretKeypair(
+      sender.secret,
+      sender.secondSecret,
+    );
+    data.senderSecondPublicKey =
+      await bfchainCore.accountBaseHelper.getPublicKeyStringFromSecondSecret(
+        sender.secret,
+        sender.secondSecret,
+      );
+  }
+  const trs = await bfchainCore.transaction.createTransaction<AcceptVoteTransaction>(
+    AcceptVoteTransactionFactory,
+    data,
+    {},
+    keypair,
+    secondKeypair,
+  );
+  return trs.toJSON();
+}
+
+async function getPromiseTransaction(
+  sender: AccountModel,
+  transaction: BFChainCore.TransactionJSON,
+  bfchainCore: BFChainCore,
+) {
+  const keypair = await bfchainCore.accountBaseHelper.createSecretKeypair(sender.secret);
+  const data: BFChainCore.TxBodyJSON = {
+    version: bfchainCore.config.version,
+    type: bfchainCore.transactionHelper.PROMISE, // 交易类型
+    senderId: sender.address, // 发起者地址
+    recipientId: transaction.senderId,
+    senderPublicKey: sender.publicKey, // 发起者公钥
+    senderSecondPublicKey: "", // 发起者二次公钥
+    rangeType: RANGE_TYPE.EMPTY,
+    range: [],
+    timestamp: 770880, // 生成交易时间戳
+    fee: "10000", // 交易手续费
+    remark: { remark: "create promise" }, // 交易备注，任意信息
+    dappid: getRandomDAppid(), // 交易所属的 dappid
+    lns: bfchainCore.config.genesisLocationName,
+    sourceIP: "127.0.0.1", // 交易来源 ip
+    fromMagic: bfchainCore.config.magic, // 交易来源链的 magic
+    toMagic: bfchainCore.config.magic, // 交易去往链的 magic
+    applyBlockHeight: 10086, // 交易发起高度
+    effectiveBlockHeight: 10100,
+  };
+  let secondKeypair;
+  if (sender.secondSecret) {
+    secondKeypair = await bfchainCore.accountBaseHelper.createSecondSecretKeypair(
+      sender.secret,
+      sender.secondSecret,
+    );
+    data.senderSecondPublicKey =
+      await bfchainCore.accountBaseHelper.getPublicKeyStringFromSecondSecret(
+        sender.secret,
+        sender.secondSecret,
+      );
+  }
+  const trs = await bfchainCore.transaction.createTransaction<PromiseTransaction>(
+    PromiseTransactionFactory,
+    data,
+    {
+      promise: { transaction },
+    },
+    keypair,
+    secondKeypair,
+  );
+
+  const trsJson = trs.toJSON();
+  const xx = await bfchainCore.transaction.recombineTransaction<PromiseTransaction>(trsJson);
+
+  const factory = bfchainCore.transaction.getTransactionFactoryFromType(xx.type);
+
+  await factory.verifySignature(xx);
+
+  console.log(xx.toJSON());
+}
+
+(async () => {
+  try {
+    const bfchainCore = await getBfchainCoreEntry();
+
+    await getPromiseTransaction(
+      getSenderWithoutSecondSecret(),
+      await getTransferAssetTransaction(getSenderWithoutSecondSecret(), bfchainCore),
+      bfchainCore,
+    );
+    await getPromiseTransaction(
+      getSenderWithoutSecondSecret(),
+      await getAcceptVoteTransaction(getSenderWithoutSecondSecret(), bfchainCore),
+      bfchainCore,
+    );
+  } catch (error) {
+    console.log(error);
+  }
+})();
