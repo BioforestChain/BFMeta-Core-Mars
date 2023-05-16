f040cb3ba33a0a4f631f3ea815de216917c6654b
Gaubee <kezhaofeng@bnqkl.cn>
2023-05-16 21:19:04 +0800

    :sparkles: 初步为calcInput增加了表达式的执行功能

--- packages/model-transaction-complex/src/atom_input/calcInput.ts
+++ packages/model-transaction-complex/src/atom_input/calcInput.ts
@@ -1,5 +1,6 @@
 import { Message, Field, Type } from "@bfchain/protobuf";
 import { NumberInputModel } from "./numberInput";
+import * as calc from "@bnqkl/calc";
 
 @Type.d("CalcInputModel")
 export class CalcInputModel
@@ -14,5 +15,19 @@ export class CalcInputModel
       calc: this.calc,
     };
     return resp;
+  }
+  // /**
+  //  * 编译成 js 函数
+  //  */
+  // @cacheGetter
+  // private _calc() {
+  //   new calc.Parser(this.calc)
+  // }
+  /**
+   * 执行函数，获得输出
+   * @param params
+   */
+  evaluate(params: calc.$Scope) {
+    return calc.evaluate(this.calc, params);
   }
 }
