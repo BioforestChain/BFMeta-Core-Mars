76f447dfcf23ac7fbd1f5e8b2ab69fab544739b2
Gaubee <kezhaofeng@bnqkl.cn>
2023-05-16 22:30:29 +0800

    :sparkles: 实现了 MacroCall 生成交易的具体实现

--- packages/model-transaction-complex/src/atom_input/calcInput.ts
+++ packages/model-transaction-complex/src/atom_input/calcInput.ts
@@ -1,28 +1,25 @@
 import { Message, Field, Type } from "@bfchain/protobuf";
 import { NumberInputModel } from "./numberInput";
 import * as calc from "@bnqkl/calc";
+import { MACRO_CALC_PRECISION, MACRO_INPUT_TYPE } from "./constants";
 
 @Type.d("CalcInputModel")
 export class CalcInputModel
-  extends NumberInputModel
+  extends NumberInputModel<MACRO_INPUT_TYPE.CALC>
   implements BFChainCore.AssetJSONToModelType<BFChainCore.Macro.CalcInputJSON>
 {
   @Field.d(CalcInputModel.INC++, "string")
   calc!: string;
+  @Field.d(CalcInputModel.INC++, "string")
+  precision!: MACRO_CALC_PRECISION;
   toJSON() {
     const resp: BFChainCore.Macro.CalcInputJSON = {
       ...super.toJSON(),
       calc: this.calc,
+      precision: this.precision,
     };
     return resp;
   }
-  // /**
-  //  * 编译成 js 函数
-  //  */
-  // @cacheGetter
-  // private _calc() {
-  //   new calc.Parser(this.calc)
-  // }
   /**
    * 执行函数，获得输出
    * @param params
