ef1812be79f66710c2ef37b697d2e47a5b9d5651
wmc <wengmaochun@bnqkl.cn>
2023-05-25 15:10:28 +0800

    :bug: 修改事件监听机制

--- packages/transaction_complex_logic_verifier/src/promiseResolveLogicVerifier.ts
+++ packages/transaction_complex_logic_verifier/src/promiseResolveLogicVerifier.ts
@@ -1,11 +1,11 @@
 import type { PromiseResolveTransaction } from "@bfchain/core-model";
+import { Injectable, Inject, QueneEventEmitter } from "@bfchain/util";
+import { TransactionCore } from "@bfchain/core-transaction";
 import {
   TransactionLogicVerifier,
   TransactionLogicVerifierCore,
 } from "@bfchain/core-transaction-logic-verifier";
-import { Injectable, Inject, QueneEventEmitter } from "@bfchain/util";
 import { CoreExceptionGenerator, ERROR_LIST } from "@bfchain/core-util-exception";
-import { TransactionCore } from "@bfchain/core-transaction";
 
 const { ConsensusException, NoFoundException } = CoreExceptionGenerator(
   "VERIFIER",
@@ -26,32 +26,33 @@ export class PromiseResolveLogicVerifier extends TransactionLogicVerifier {
   async verify(
     transaction: PromiseResolveTransaction,
     currentBlockHeight: number,
-    accountsInfo: {
-      sender: BFChainCore.AccountInfoAndAssets;
-      recipient?: BFChainCore.AccountInfoAndAssets;
-    },
+    accountMap: Map<string, BFChainCore.AccountInfoAndAssets>,
     accountGetterHelper: BFChainCore.AccountGetterHelperInterface,
     transactionGetterHelper: BFChainCore.TransactionGetterHelperInterface,
+    skipListenEvent = false,
   ) {
-    const { sender, recipient } = await this.logicVerify(
+    await this.logicVerify(
       transaction,
       currentBlockHeight,
-      accountsInfo,
+      accountMap,
       accountGetterHelper,
       transactionGetterHelper,
     );
 
-    const { senderId, recipientId } = transaction;
-    const cloneAccountsAssets = {
-      [transaction.senderId]: this.helperLogicVerifier.deepClone(sender.accountAssets),
-    };
-    const cloneAccountsInfo = {
-      [transaction.senderId]: this.helperLogicVerifier.deepClone(sender.accountInfo),
-    };
-
     const eventEmitter = new QueneEventEmitter() as BFChainCore.ApplyTransactionEventEmitter;
 
     const { eventLogicVerifier } = this;
+
+    if (skipListenEvent === false) {
+      eventLogicVerifier.listenEvent(
+        accountMap,
+        currentBlockHeight,
+        accountGetterHelper,
+        eventEmitter,
+      );
+    }
+
+    await eventLogicVerifier.awaitEventResult(transaction, eventEmitter);
 
     const { promiseId } = transaction.asset.resolve;
     const promiseTransactionJson = await transactionGetterHelper.getPromiseTransaction(promiseId);
@@ -72,63 +73,20 @@ export class PromiseResolveLogicVerifier extends TransactionLogicVerifier {
         target: "blockChain",
       });
     }
-
-    eventLogicVerifier.listenEventFee(cloneAccountsAssets, eventEmitter);
-
-    const accountMap = new Map<string, BFChainCore.AccountInfoAndAssets>([[senderId, sender]]);
-    if (recipientId && recipient) {
-      accountMap.set(recipientId, recipient);
-    }
-
     const logicVerify = this.transactionLogicVerifierCore.getTransactionLogicVerifierFromType(
       promiseTransactionJson.type,
     );
-    const { senderId: subSenderId, recipientId: subRecipientId } = promiseTransactionJson;
-    let subSender = accountMap.get(subSenderId);
-    if (!subSender) {
-      const result = await accountGetterHelper.getAccountInfoAndAssets(
-        senderId,
-        currentBlockHeight,
-      );
-      if (!result) {
-        throw new ConsensusException(ERROR_LIST.NOT_EXIST, {
-          prop: `Account with address ${subSenderId}`,
-          target: "blockChain",
-        });
-      }
-      subSender = result;
-      accountMap.set(subSenderId, subSender);
-    }
-    let subRecipient: BFChainCore.AccountInfoAndAssets | undefined;
-    if (subRecipientId) {
-      subRecipient = accountMap.get(subRecipientId);
-      if (!subRecipient) {
-        const result = await accountGetterHelper.getAccountInfoAndAssets(
-          subRecipientId,
-          currentBlockHeight,
-        );
-        if (!result) {
-          throw new ConsensusException(ERROR_LIST.NOT_EXIST, {
-            prop: `Account with address ${subSenderId}`,
-            target: "blockChain",
-          });
-        }
-        subRecipient = result;
-        accountMap.set(subRecipientId, subRecipient);
-      }
-    }
     const promiseTransaction = await this.transactionCore.recombineTransaction(
       promiseTransactionJson,
     );
     await logicVerify.verify(
       promiseTransaction,
       currentBlockHeight,
-      { sender: subSender, recipient: subRecipient },
+      accountMap,
       accountGetterHelper,
       transactionGetterHelper,
+      true,
     );
-
-    await eventLogicVerifier.awaitEventResult(transaction, eventEmitter);
 
     return true;
   }
